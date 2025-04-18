9a205abedb873f9a526877ed8d36278bb6ec6c9b
jabelopitso <159244055+jabelopitso@users.noreply.github.com>
2025-04-18 04:19:35 +0200

    add my code

---
+++ basic_calculator.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a -b
+
+def multiply(a,b):
+    return a * b
+
+def division(a,b):
+    return a/ b
+
+def divide(a,b):
+    if a and b % 0:
+        return ZeroDivisionError
+    
+    
